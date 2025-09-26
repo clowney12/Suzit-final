f70d4f386f051377466912ebf46807029e5fa17d
TARUN KUMAR <clownsellor@gmail.com>
2025-09-26 15:58:57 +0000

    Insert this picture in John doe picture change the format of the image i

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
