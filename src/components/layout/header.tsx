2178f7558401448b1706a060d508eed06c14b558
TARUN KUMAR <clownsellor@gmail.com>
2025-09-26 15:53:58 +0000

    Remove Portfolio page

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,7 +12,6 @@ const navLinks = [
   { href: '/', label: 'Home' },
   { href: '/about', label: 'About' },
   { href: '/services', label: 'Services' },
-  { href: '/portfolio', label: 'Portfolio' },
   { href: '/contact', label: 'Contact' },
 ];
 
